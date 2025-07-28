0ee3d2d21da8bf1c41466ab703cb99593b3894a5
kalai85 <kalaiyarasic923@gmail.com>
2025-07-28 08:36:14 +0530

    Create task2.js

---
+++ day 3/task2.js
@@ -0,0 +1,10 @@
+function double(x){
+    return x* 2;
+}
+const doubleExp = function(x){
+    return x*2;
+}
+const doubleArrow=x=>x*2;
+console.log(double(5));
+console.log(doubleExp(15));
+console.log(doubleArrow(30));
